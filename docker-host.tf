06aabd2d1f36e1f7aed990f262b076b3d13a95b3
aracloud <aracloud@gmx.net>
2026-08-27 14:14:47 +0200

    changed

--- docker-host.tf
+++ docker-host.tf
@@ -23,8 +23,6 @@ resource "aws_network_interface" "aws_nic_dkr" {
 
 
 resource "aws_instance" "aws_dkr" {
-  name = "${var.prefix}-dkr-node"
-
   ami = data.aws_ami.debian.id
   instance_type = var.docker-instance-type
 

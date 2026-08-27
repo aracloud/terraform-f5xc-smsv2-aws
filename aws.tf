d6f73b5c64117d9bf585c348ca74503ac6521c59
aracloud <aracloud@gmx.net>
2026-08-27 14:45:17 +0200

    changed

--- aws.tf
+++ aws.tf
@@ -1,80 +1,150 @@
 ####################################
-# AWS resource definitions
+# AWS VPC
 
-resource "aws_vpc" "aws_vpc" {
-  cidr_block = "10.0.0.0/16"
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name   = "${var.prefix}-network"
+    Name   = "${var.prefix}-vpc"
     source = var.tag_source_git
     owner  = var.tag_owner
-    host   = local.hostname
-    create = local.today-timestamp
   }
 }
 
-resource "aws_subnet" "aws_sn" {
-  vpc_id            = aws_vpc.aws_vpc.id
-  cidr_block        = "10.0.2.0/24"
-  availability_zone = var.aws_availability_zone
+
+####################################
+# Internet Gateway
+
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name   = "${var.prefix}-sn-internal"
+    Name   = "${var.prefix}-igw"
     source = var.tag_source_git
     owner  = var.tag_owner
-    host   = local.hostname
-    create = local.today-timestamp
   }
 }
 
-resource "aws_security_group" "aws_sg" {
-  name        = "${var.prefix}-sg"
-  description = "Security Group for F5XC CE"
-  vpc_id      = aws_vpc.aws_vpc.id
 
-  ingress {
-    description = "Allow SSH"
-    protocol    = "tcp"
-    from_port   = 22
-    to_port     = 22
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+####################################
+# SLO subnet
+# F5XC outside interface
 
-  ingress {
-    description = "Allow TCP 65500"
-    protocol    = "tcp"
-    from_port   = 65500
-    to_port     = 65500
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "Allow ICMP"
-    protocol    = "icmp"
-    from_port   = -1
-    to_port     = -1
-    cidr_blocks = ["0.0.0.0/0"]
-  }
+resource "aws_subnet" "slo" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.101.0/24"
+  availability_zone       = var.aws_availability_zone
+  map_public_ip_on_launch = false
 
   tags = {
-    Name   = "${var.prefix}-sg"
+    Name   = "${var.prefix}-slo"
     source = var.tag_source_git
     owner  = var.tag_owner
-    host   = local.hostname
-    create = local.today-timestamp
   }
 }
 
-resource "aws_route_table" "aws_rt" {
-  vpc_id = aws_vpc.aws_vpc.id
+
+####################################
+# SLI subnet
+# F5XC inside interface
+
+resource "aws_subnet" "sli" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.102.0/24"
+  availability_zone       = var.aws_availability_zone
+  map_public_ip_on_launch = false
 
   tags = {
-    Name = "${var.prefix}-rt"
+    Name   = "${var.prefix}-sli"
+    source = var.tag_source_git
+    owner  = var.tag_owner
   }
 }
 
-resource "aws_route_table_association" "aws_rta" {
-  subnet_id      = aws_subnet.aws_sn.id
-  route_table_id = aws_route_table.aws_rt.id
+
+####################################
+# Backend subnet
+# Docker / application workloads
+
+resource "aws_subnet" "backend" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.103.0/24"
+  availability_zone       = var.aws_availability_zone
+  map_public_ip_on_launch = false
+
+  tags = {
+    Name   = "${var.prefix}-backend"
+    source = var.tag_source_git
+    owner  = var.tag_owner
+  }
 }
 
+
+####################################
+# Public route table
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+
+  tags = {
+    Name   = "${var.prefix}-public-rt"
+    source = var.tag_source_git
+    owner  = var.tag_owner
+  }
+}
+
+
+####################################
+# SLO route table association
+
+resource "aws_route_table_association" "slo" {
+  subnet_id      = aws_subnet.slo.id
+  route_table_id = aws_route_table.public.id
+}
+
+
+####################################
+# SLI route table
+
+resource "aws_route_table" "sli" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name   = "${var.prefix}-sli-rt"
+    source = var.tag_source_git
+    owner  = var.tag_owner
+  }
+}
+
+
+resource "aws_route_table_association" "sli" {
+  subnet_id      = aws_subnet.sli.id
+  route_table_id = aws_route_table.sli.id
+}
+
+
+####################################
+# Backend route table
+
+resource "aws_route_table" "backend" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name   = "${var.prefix}-backend-rt"
+    source = var.tag_source_git
+    owner  = var.tag_owner
+  }
+}
+
+
+resource "aws_route_table_association" "backend" {
+  subnet_id      = aws_subnet.backend.id
+  route_table_id = aws_route_table.backend.id
+}
--- xc-smsv2.tf
+++ xc-smsv2.tf
@@ -86,27 +86,6 @@ resource "aws_eip_association" "f5xc_ce" {
   allocation_id = aws_eip.f5xc_ce.id
 }
 
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.aws_vpc.id
-
-  tags = {
-    Name = "${var.prefix}-igw"
-  }
-}
-
-resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.aws_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.main.id
-  }
-
-  tags = {
-    Name = "${var.prefix}-rt-public"
-  }
-}
-
 resource "aws_route_table_association" "public" {
   subnet_id      = aws_subnet.aws_sn.id
   route_table_id = aws_route_table.public.id

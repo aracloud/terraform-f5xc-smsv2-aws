dc54cc159944c03d91ca74a54f9a24da42d72aea
aracloud <aracloud@gmx.net>
2026-08-28 14:55:41 +0200

    change

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,22 @@ output "hostname" {
 output "sitename" {
   value = local.smsv2-site-name
 }
+
+output "f5xc_slo_nic" {
+  description = "F5 XC Node SLO (Outside) Interface Details"
+  value = {
+    id         = aws_network_interface.f5xc_slo.id
+    private_ip = aws_network_interface.f5xc_slo.private_ip
+    public_ip  = aws_eip_association.f5xc_slo.public_ip
+    subnet_id  = aws_network_interface.f5xc_slo.subnet_id
+  }
+}
+
+output "f5xc_sli_nic" {
+  description = "F5 XC Node SLI (Inside) Interface Details"
+  value = {
+    id         = aws_network_interface.f5xc_sli.id
+    private_ip = aws_network_interface.f5xc_sli.private_ip
+    subnet_id  = aws_network_interface.f5xc_sli.subnet_id
+  }
+}

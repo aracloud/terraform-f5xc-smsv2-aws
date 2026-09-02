46b8b7a2ac9e6df3b6f3f848f83d171aaceb0841
aracloud <aracloud@gmx.net>
2026-09-02 09:06:26 +0200

    fix: add ssh_docker_host and docker_nic outputs to configuration

--- outputs.tf
+++ outputs.tf
@@ -6,6 +6,22 @@ output "sitename" {
   value = local.smsv2-site-name
 }
 
+output "ssh_docker_host" {
+  description = "SSH Command fuer den Docker-Host"
+  value       = "ssh -i ~/.ssh/id_rsa admin@${aws_instance.docker_host.public_ip}"
+}
+
+output "docker_nic" {
+  description = "Docker Host Network Interface Details"
+  value = {
+    eni_id     = aws_network_interface.aws_nic_dkr.id
+    private_ip = aws_network_interface.aws_nic_dkr.private_ip
+    subnet_id  = aws_network_interface.aws_nic_dkr.subnet_id
+    # Falls eine EIP / Public IP vorhanden ist:
+    public_ip = try(aws_instance.aws_dkr.public_ip, "None")
+  }
+}
+
 output "f5xc_slo_nic" {
   description = "F5 XC Node SLO (Outside) Interface Details"
   value = {
@@ -22,16 +38,5 @@ output "f5xc_sli_nic" {
     id         = aws_network_interface.f5xc_sli.id
     private_ip = aws_network_interface.f5xc_sli.private_ip
     subnet_id  = aws_network_interface.f5xc_sli.subnet_id
-  }
-}
-
-output "docker_nic" {
-  description = "Docker Host Network Interface Details"
-  value = {
-    eni_id     = aws_network_interface.aws_nic_dkr.id
-    private_ip = aws_network_interface.aws_nic_dkr.private_ip
-    subnet_id  = aws_network_interface.aws_nic_dkr.subnet_id
-    # Falls eine EIP / Public IP vorhanden ist:
-    public_ip = try(aws_instance.aws_dkr.public_ip, "None")
   }
 }
